4bd9fa78f9ac8709a4a2716c63d765ba4e7b1ff2
Martin <myspammbox@seznam.cz>
2020-09-03 09:58:55 +0200

    Initial commit

---
+++ app/src/main/java/opkp/solutions/guesstheanimal/MainActivity.kt
@@ -0,0 +1,11 @@
+package opkp.solutions.guesstheanimal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
