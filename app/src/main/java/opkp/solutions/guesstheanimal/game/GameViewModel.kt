656e9b312713637484f10da7ebe8124acea47701
Martin <myspammbox@seznam.cz>
2020-09-08 10:50:38 +0200

    game_fragment created

--- app/src/main/java/opkp/solutions/guesstheanimal/game/GameViewModel.kt
+++ app/src/main/java/opkp/solutions/guesstheanimal/game/GameViewModel.kt
@@ -5,6 +5,12 @@ import opkp.solutions.guesstheanimal.dataclass.Animal
 
 class GameViewModel {
 
+
+    val picture1 = Int
+    val picture2 = Int
+    val picture3 = Int
+    val picture4 = Int
+
     private val animalList: MutableList<Animal> = mutableListOf(
         Animal("buffalo", R.drawable.ic_buffalo, R.raw.buffalo),
         Animal("canarybird", R.drawable.ic_canary_bird, R.raw.canarybird),
@@ -20,11 +26,22 @@ class GameViewModel {
         Animal("owl", R.drawable.ic_owl, R.raw.owl),
         Animal("pig", R.drawable.ic_pig, R.raw.pig),
         Animal("rhinoceros", R.drawable.ic_rhinoceros, R.raw.rhinoceros),
-        Animal("rooster", R.drawable.ic_seagull, R.raw.seagull),
-        Animal("seagull", R.drawable.ic_sheep, R.raw.sheeps),
-        Animal("sheeps", R.drawable.ic_tiger, R.raw.tiger),
-        Animal("tiger", R.drawable.ic_turkey, R.raw.turkey),
-        Animal("turkey", R.drawable.ic_wolf, R.raw.wolf),
-        Animal("wolf", R.drawable.ic_canary_bird, R.raw.canarybird)
+        Animal("rooster", R.drawable.ic_rooster, R.raw.rooster),
+        Animal("seagull", R.drawable.ic_seagull, R.raw.seagull),
+        Animal("sheeps", R.drawable.ic_sheep, R.raw.sheeps),
+        Animal("tiger", R.drawable.ic_tiger, R.raw.tiger),
+        Animal("turkey", R.drawable.ic_turkey, R.raw.turkey),
+        Animal("wolf", R.drawable.ic_wolf, R.raw.wolf)
     )
+    private fun pickAnimals() {
+        animalList.shuffle()
+        if (animalList.size >= 4) {
+            //TODO choose 4 pictures from list as picture1-4 and remove them from list
+        } else {
+            //TODO Navcontroller GameFragment -> GameOverFragment
+        }
+    }
+
+
+
 }
